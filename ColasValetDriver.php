630d8d1e11f75c653f2a8e27b0a25cd97db508dc
Yudhi Satrio <isatrio.is.me@gmail.com>
2021-10-08 16:09:42 +0700

    First commit

---
+++ ColasValetDriver.php
@@ -0,0 +1,60 @@
+<?php
+
+class ColasValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        if (file_exists($sitePath.'/Dailymotion.php')) {
+          return true; 
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath.'/public/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+       $url_param = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+       if ( strpos($url_param, 'css') === false && 
+         strpos($url_param, 'img') === false && 
+         strpos($url_param, 'fonts') === false ) {
+         return $sitePath.'/controller.php';
+       }
+
+        return $sitePath . $uri;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# Colas Garage Valet Driver
+
+This driver is to make Colas Garage working on local machine using valet.
+The idea is just to repointing default index.php to controller.php. If
+you're using another, just do the pointing to the right folder and right
+file "controller.php".
+
+To use this, just copy the `ColasValetDriver` to correct valet folder.
+
+## Mac
+
+`~/.config/valet/Drivers/`
+
+## Linux
